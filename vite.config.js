4b41e35bab7ab062672f54341ec60bf0cfb527c2
syedharis14 <sharisdev08@gmail.com>
2025-03-28 04:51:42 +0500

    refactor(app): update homepage to use ResumePage component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    base: "/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "#components": resolve(__dirname, "src/components"),
+            "#page": resolve(__dirname, "src/page")
+        }
+    }
+});
