a2781e35a1bf4cb768a98e321ca1391c122e941d
syedharis14 <sharisdev08@gmail.com>
2025-03-28 05:43:23 +0500

    chore(deps): add gh-pages and new dev dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/",
+    base: "/resume/",
     plugins: [vue()],
     resolve: {
         alias: {
